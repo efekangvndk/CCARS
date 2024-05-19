e5b87edc9d1d0a485f00c48c811916f3fc0c82b0
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-05-20 00:50:58 +0300

    ss

---
+++ CCARS/CCARS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CCARS
+//
+//  Created by Efekan Güvendik on 20.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
