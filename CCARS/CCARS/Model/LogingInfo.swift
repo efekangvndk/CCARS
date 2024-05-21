cb5da7338993e64519f21bc739ad20f7f06225bb
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-05-21 15:54:55 +0300

    Setup FireBase

---
+++ CCARS/CCARS/Model/LogingInfo.swift
@@ -0,0 +1,14 @@
+//
+//  LogingInfo.swift
+//  CCARS
+//
+//  Created by Efekan Güvendik on 21.05.2024.
+//
+
+import UIKit
+
+struct LogingInfo {
+    
+    let nickName : String
+    let passWord : Int
+}
